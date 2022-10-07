9d8a519dc5fb6d746a50eed0b2d201a8c15bae03
Michael Hughes <Michaelhughes5310@gmail.com>
2022-10-07 13:14:27 -0400

    Added a check winner function. Still need to debug it because no matter the outcome it is showing 'tie'

--- javascript.js
+++ javascript.js
@@ -13,17 +13,31 @@ let computerChoice = 'place holder';
 // create a function called getComputerChoice to randomly return Rock, Paper, Sciccors
 
 function getComputerChoice() {
-  let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+  computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
   return computerChoice;
 }
 
 // create a function to play a 'round' of rock, paper, scissors
 
 function playRound(playerChoice, computerChoice) {
-  playerChoice = prompt("Please pick: rock, paper, or scissors");
-  return playerChoice.toLowerCase()
-} if (playerChoice === "rock" || computerChoice === "rock") {
-    console.log("You both chose rock! it's a tie!")
+  playerChoice = prompt("Please pick: rock, paper, or scissors")
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = getComputerChoice()
+  return console.log(`you chose ${playerChoice} and the computer chose ${computerChoice}!`), checkWinner();
+} 
+
+// create a function to check winner
+
+function checkWinner() {
+  if (playerChoice === 'rock' && computerChoice === 'scissors') {
+      console.log('You win! Congratulations!')
+  } 
+  else if (playerChoice === 'rock' && computerChoice === 'paper') {
+      console.log('That means you lose!')
+  } 
+  else {
+      console.log("It's a tie!")
+  }
 }
 
 console.log(playRound())
