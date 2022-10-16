a8f464cc793f3deec982c28619aef7e39bf63448
Michael Hughes <Michaelhughes5310@gmail.com>
2022-10-15 21:51:20 -0400

    Got most of the UI created now need to get Javascript working. kicking my but right now. DOM manipulation...

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
 // create an array to store the options for getComputerChoice to pick from
 
-let computerOptions = ["rock", "paper", "scissors"];
+let computerOptions = [ "fire", "water", "grass"];
 
 // create a variable called playerChoice to store the players and prompt them for their selection
 
 let playerChoice = "place holder";
+let playerChoicePic = document.querySelector("#player-choice-img")
+
+
 
 // create a variable to store my getComputer choice in
 
@@ -19,7 +22,7 @@ let winStatus = "win"
 let computerScore = 0
 let playerScore = 0
 
-// create a function called getComputerChoice to randomly return Rock, Paper, Sciccors
+// create a function called getComputerChoice to randomly return fire, water, Sciccors
 
 function getComputerChoice() {
   computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
@@ -29,16 +32,30 @@ function getComputerChoice() {
 // create a function to get playerchoice
 
 function getPlayerChoice() {
-  playerChoice = prompt("Please pick: rock, paper, or scissors")
+  playerChoice = prompt("Please pick: fire, water, or grass")
   playerChoice = playerChoice.toLowerCase();
   return playerChoice
 }
 
-// create a function to play a 'round' of rock, paper, scissors
+function generatePlayerPic() {
+  if (playerChoice === 'fire') {
+    playerChoicePic = document.querySelector("#player-choice-img").innerHTML = '<img src="/images/fire-type.png"/>';
+  } if (playerChoice === 'water') {
+    playerChoicePic = document.querySelector("#player-choice-img").innerHTML = '<img src="/images/water-type.png"/>';
+  } if (playerChoice === "grass") {
+     playerChoicePic = document.querySelector("#player-choice-img").innerHTML = '<img src="/images/grass-type.png"/>';
+  }
+  return playerChoicePic;
+
+  }
+
+
+// create a function to play a 'round' of fire, water, grass
 
 function playRound(playerChoice, computerChoice) {
   computerChoice = getComputerChoice();
   playerChoice = getPlayerChoice();
+  generatePlayerPic();
   checkWinner();
   return console.log(`you chose ${playerChoice} and the computer chose ${computerChoice}, ${winStatus}! The score is ${playerScore} to ${computerScore}!`);
 } 
@@ -46,30 +63,30 @@ function playRound(playerChoice, computerChoice) {
 // create a function to check winner
 
 function checkWinner() {
-  if (playerChoice === "rock") {
-    if (computerChoice === "rock") {
+  if (playerChoice ===  "fire") {
+    if (computerChoice ===  "fire") {
       winStatus = "it's a tie!"
-    } else if (computerChoice === "scissors") {
+    } else if (computerChoice === "grass") {
       winStatus = "you win!"
       playerScore = ++playerScore
     } else {
       winStatus = " you lose!"
       computerScore = ++computerScore
     }
-  } else if (playerChoice === "paper") {
-    if (computerChoice === "paper") {
+  } else if (playerChoice === "water") {
+    if (computerChoice === "water") {
       winStatus = "it's a tie!";
-    } else if (computerChoice === "rock") {
+    } else if (computerChoice ===  "fire") {
       winStatus = "you win!";
       playerScore = ++playerScore
     } else {
       winStatus = "you lose!";
       computerScore = ++computerScore;
     }
-  } else if (playerChoice === "scissors") {
-    if (computerChoice === "scissors") {
+  } else if (playerChoice === "grass") {
+    if (computerChoice === "grass") {
       winStatus = "it's a tie!";
-    } else if (computerChoice === "paper") {
+    } else if (computerChoice === "water") {
       winStatus = "you win!";
       playerScore = ++playerScore
     } else {
@@ -85,14 +102,10 @@ function startGame() {
   playRound();
 }
 
-console.log(startGame())
+// DOM manipulation
 
-
-
-
-
-
-
-
-
-
+function windowOnClick(event) {
+  if (event.target === body) {
+    startGame();
+  }
+}
